202cde19d095522ef1cfa215714cc388ea21c978
LazyPanda07 <semengricenko@gmail.com>
2022-02-26 16:56:17 +0500

    Change circle field

--- Game/src/Fields/CircleField.cpp
+++ Game/src/Fields/CircleField.cpp
@@ -1,8 +1,28 @@
 #include "CircleField.h"
 
-#include <iostream>
-
 using namespace std;
+
+namespace fields
+{
+	namespace circle_utility
+	{
+		int64_t checkSign(const point& firstPoint, const point& secondPoint, const point& thirdPoint)
+		{
+			return (firstPoint.x - thirdPoint.x) * (secondPoint.y - thirdPoint.y) - (secondPoint.x - thirdPoint.x) * (firstPoint.y - thirdPoint.y);
+		}
+
+		bool isPointInTriangle(const point& checkPoint, const triangle& triangle)
+		{
+			int64_t v1v2Check = checkSign(checkPoint, triangle.v1, triangle.v2);
+			int64_t v2v3Check = checkSign(checkPoint, triangle.v2, triangle.v3);
+			int64_t v3v1Check = checkSign(checkPoint, triangle.v3, triangle.v1);
+			bool hasNeg = (v1v2Check < 0) || (v2v3Check < 0) || (v3v1Check < 0);
+			bool hasPos = (v1v2Check > 0) || (v2v3Check > 0) || (v3v1Check > 0);
+
+			return !(hasNeg && hasPos);
+		}
+	}
+}
 
 namespace fields
 {
@@ -11,13 +31,28 @@ namespace fields
 		vector<vector<int>> circle(height, vector<int>(width, -1));
 		int64_t centerX = radius;
 		int64_t centerY = radius;
-		int64_t squareRadius = static_cast<int64_t>(pow(radius, 2));
+
+		auto checkCirclePosition = [this, &centerX, &centerY](int64_t x, int64_t y)
+		{
+			using namespace circle_utility;
+
+			static triangle topLeft = { { centerX, centerY }, { 0, centerY }, { centerX, 0 } };
+			static triangle topRight = { { centerX, centerY }, { static_cast<int64_t>(width) - 1, centerY }, { centerX, 0 } };
+			static triangle bottomLeft = { { centerX, centerY }, { 0, centerY }, { centerX, static_cast<int64_t>(height) - 1 } };
+			static triangle bottomRight = { { centerX, centerY }, { static_cast<int64_t>(width) - 1 , centerY }, { centerX, static_cast<int64_t>(height) - 1 } };
+			point checkPoint = { x, y };
+
+			return isPointInTriangle(checkPoint, topLeft) ||
+				isPointInTriangle(checkPoint, topRight) ||
+				isPointInTriangle(checkPoint, bottomLeft) ||
+				isPointInTriangle(checkPoint, bottomRight);
+		};
 
 		for (int64_t y = 0; y < static_cast<int64_t>(height); y++)
 		{
 			for (int64_t x = 0; x < static_cast<int64_t>(width); x++)
 			{
-				if ((pow(x - centerX, 2) + pow(y - centerY, 2)) <= squareRadius)
+				if (checkCirclePosition(x, y))
 				{
 					circle[y][x] = 0;
 				}
@@ -31,7 +66,7 @@ namespace fields
 		BaseField(radius * 2 + 1, radius * 2 + 1),
 		radius(radius)
 	{
-		
+
 	}
 
 	size_t CircleField::getRadius() const
--- Game/src/Fields/CircleField.h
+++ Game/src/Fields/CircleField.h
@@ -4,6 +4,26 @@
 
 namespace fields
 {
+	namespace circle_utility
+	{
+		struct point
+		{
+			int64_t x;
+			int64_t y;
+		};
+
+		struct triangle
+		{
+			point v1;
+			point v2;
+			point v3;
+		};
+
+		int64_t checkSign(const point& firstPoint, const point& secondPoint, const point& thirdPoint);
+
+		bool isPointInTriangle(const point& checkPoint, const triangle& triangle);
+	}
+
 	class CircleField : public BaseField
 	{
 	protected:
